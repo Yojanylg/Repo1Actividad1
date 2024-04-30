03d2e5e05c593c94fb381f883319a94e7229b878
Yojanylg <yojanylg@gmail.com>
2024-04-30 17:19:21 +0200

    add clase Yojany

---
+++ src/yojany/Yojany.java
@@ -0,0 +1,12 @@
+package yojany;
+
+public class Yojany {
+
+	public static void main(String [] args)	{
+		imprimirNombre();
+	}
+
+	static void imprimirNombre() {
+	System.out.println("Hola, soy Yojany L G");
+	}
+}
