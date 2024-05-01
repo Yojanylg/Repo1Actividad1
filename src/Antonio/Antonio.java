a59d30317c3e0c3bee7b9a0a108e0a8fe80561f3
tonyMS90 <martinezseguratony@gmail.com>
2024-05-01 11:43:19 +0200

    develop1 ha hecho cambios y ha creado un paquete nuevo con una clase

---
+++ src/Antonio/Antonio.java
@@ -0,0 +1,13 @@
+package Antonio;
+
+public class Antonio {
+	
+	public static void main(String [] args)	{
+		imprimirNombre();
+	}
+
+	static void imprimirNombre() {
+	System.out.println("Hola, soy Antonio M.S");
+	}
+
+}
