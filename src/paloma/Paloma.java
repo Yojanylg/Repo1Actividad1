f1f13dc6a77a531cf061b46aff4c1f82f3582b8b
Paloma de los Milagros <palomadlmm@gmail.com~>
2024-05-01 15:00:45 +0200

    develop2 ha creado un paquete nuevo con una clase

---
+++ src/paloma/Paloma.java
@@ -0,0 +1,19 @@
+package paloma;
+
+public class Paloma {
+
+	public static void main(String[] args) {
+		
+			imprimirNombre();
+		}
+
+	static void imprimirNombre() {
+	System.out.println("Hola, soy Paloma de los Milagros Martínez");
+	}
+
+
+}
+
+	
+
+
