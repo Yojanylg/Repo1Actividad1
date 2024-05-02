a462bdb772fc1127401f60d3fc208161cf620313
Carloscmc711 <carlostoledo711gc@gmail.com>
2024-05-02 12:55:11 +0200

    Actualizacion Carlos

---
+++ src/Carlos/Carlos.java
@@ -0,0 +1,15 @@
+package Carlos;
+
+public class Carlos {
+
+	public static void main(String[] args) {
+		
+			imprimirNombre();
+		}
+
+	static void imprimirNombre() {
+	System.out.println("Hola, soy Paloma de los Milagros Martínez");
+	}
+
+
+}
